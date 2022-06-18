46f3f6df4fa485d1a506a88fb0aa96ed617441ab
Jose Goyos <josegoyos1@gmail.com>
2022-06-17 20:49:13 -0500

    cards created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,21 +4,24 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
   declarations: [
-    MenuComponent,
+    CardsComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    MenuComponent,
+    CardsComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
   ]
 })
 export class SharedModule { }
