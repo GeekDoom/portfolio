82ea28881fa41e2871c139e25d3fb7744f522f7f
Jose Goyos <josegoyos1@gmail.com>
2022-06-21 13:16:04 -0500

    side-bar done, router done

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'upload', component: UploadComponent },
       { path: '**', redirectTo: 'dashboard' }
     ]
   }
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UploadComponent } from './upload/upload.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    UploadComponent
   ],
   imports: [
     CommonModule,
